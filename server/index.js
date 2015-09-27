5c390765160b685d944e5fa5f9e5417bffb449d7
Herman Starikov <herman.starikov@uxpsystems.com>
2015-09-26 23:54:47 -0400

    add simple server

---
+++ server/index.js
@@ -0,0 +1,22 @@
+var express = require('express'),
+    app = express(),
+    users = [{
+        username: 'oleg',
+        password: 'letmein',
+        phoneNumber: '2894004192'
+    }];
+
+app.use(require('cookie-parser')());
+app.use(express.static('client'));
+
+app.get('/session', function(req, res) {
+    id = '666';
+    res.cookie('uid', id);
+    res.send('id ' + id + ' set');
+});
+
+app.get('/user', function (req, res) {
+    res.send('id is ' + req.cookies.uid);
+})
+
+app.listen(8080);
