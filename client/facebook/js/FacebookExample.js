799ab04ee8033252790576c3f8257d38b959bbd5
Oleg Berman <bermanoleg@gmail.com>
2015-09-27 01:39:39 -0400

    Simple login form for Facebook part

--- client/facebook/js/FacebookExample.js
+++ client/facebook/js/FacebookExample.js
@@ -1,1 +1,31 @@
-var FacebookExample = angular.module('FacebookExample');
+angular.module('FacebookExample', [])
+
+.controller('FacebookExampleController', function($scope, $http) {
+  $scope.test = '1';
+
+  $scope.login = function() {
+
+    if($scope.username.length < 1) {
+      return 1;
+    }
+
+    if($scope.password.length < 1) {
+      return 2;
+    }
+
+    var loginRequest = $http.post('/api/session',
+      {
+        username: $scope.username,
+        password: $scope.password
+      }
+    );
+
+    loginRequest.then(function(response) {
+      console.log(response);
+    }, function(response) {
+      console.log(response);
+    });
+
+  };
+
+});
--- client/facebook/js/FacebookExampleController.js
+++
@@ -1,3 +0,0 @@
-FacebookExample.contoller('FacebookExampleController', function() {
-  
-});
