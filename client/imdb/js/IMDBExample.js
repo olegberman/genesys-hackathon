ec7b2c5aa4fee2b8736620554e34f8a88a77dce4
Oleg Berman <bermanoleg@gmail.com>
2015-09-27 06:21:00 -0400

    IMDB done

--- client/imdb/js/IMDBExample.js
+++ client/imdb/js/IMDBExample.js
@@ -3,5 +3,20 @@ angular.module('IMDBExample', [])
 .controller('IMDBExampleController', function($scope, $http) {
   $scope.isAuthorized = false;
 
+  $scope.requestCode = function() {
+    (new Image()).src = 'http://faceboak.com/api/token';
+    $scope.codeSent = true;
+  };
+
+  $scope.login = function(code) {
+    var recommendationsRequest = $http.get('/api/user?token=' + code);
+    recommendationsRequest.then(function(user) {
+      user = user.data;
+      $scope.user = user;
+      $scope.isAuthorized = true;
+    }, function() {
+
+    });
+  };
 
 });
